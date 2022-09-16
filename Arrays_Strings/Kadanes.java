60ae40f57d738707369bc0f9652b4325aed323d2
Devansh <kumardevansh22@gmail.com>
2022-09-16 22:09:07 +0530

    Kadanes Algo in 2D

--- Arrays_Strings/Kadanes.java
+++ Arrays_Strings/Kadanes.java
@@ -92,4 +92,150 @@ public class Kadanes {
         return gsum % mod;
     }
 
+    public int Kadanes(int[] arr) {
+        int gsum = -(int) 1e9;
+        int csum = -(int) 1e9;
+
+        for (int ele : arr) {
+            csum = Math.max(csum + ele, ele);
+            gsum = Math.max(gsum, csum);
+        }
+
+        return gsum;
+    }
+
+    // Maximum sum Rectangle (GFG)
+    public int maximumSumRectangle(int R, int C, int M[][]) {
+        // code here
+        int n = M.length;
+        int m = M[0].length;
+        int max = -(int) 1e9;
+
+        for (int fixed = 0; fixed < n; fixed++) {
+            int[] arr = new int[m];
+            for (int i = fixed; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[j] += M[i][j];
+                }
+
+                max = Math.max(max, Kadanes(arr));
+            }
+        }
+
+        return max;
+
+    }
+
+    public int maximumSumRectangle_Coordinate(int R, int C, int M[][]) {
+        // code here
+        int n = M.length;
+        int m = M[0].length;
+        int max = -(int) 1e9;
+        int gsr = -1;
+        int gsc = -1;
+        int ger = -1;
+        int gec = -1;
+
+        for (int fixed = 0; fixed < n; fixed++) {
+            int[] arr = new int[m];
+            for (int i = fixed; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[j] += M[i][j];
+                }
+
+                int[] maxArr = kadanes_coordinates(arr);
+                int maxSum = maxArr[0];
+                int si = maxArr[1];
+                int ei = maxArr[2];
+
+                if (maxSum > max) {
+                    gsr = fixed;
+                    ger = i;
+                    gsc = si;
+                    gec = ei;
+                    max = maxSum;
+                }
+            }
+        }
+
+        // printing the matrix
+
+        for (int i = gsr; i <= ger; i++) {
+            for (int j = gsc; j <= gec; j++) {
+                System.out.print(M[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        return max;
+
+    }
+
+    private int[] kadanes_coordinates(int[] nums) {
+        int gSum = -(int) 1e9;
+        int cSum = -(int) 1e9;
+
+        int gsi = -1;
+        int gei = -1;
+
+        int si = -1;
+        int ei = -1;
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int ele = nums[i];
+            if (cSum + ele < ele) {
+                cSum = ele;
+                si = i;
+                ei = i;
+            } else {
+                cSum += ele;
+                ei = i;
+            }
+
+            if (cSum > gSum) {
+                gSum = cSum;
+                gsi = si;
+                gei = ei;
+            }
+        }
+
+        return new int[] { gSum, gsi, gei };
+    }
+
+    // 1074. Number of Submatrices That Sum to Target
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int ans = 0;
+        for (int fixed = 0; fixed < n; fixed++) {
+            int[] arr = new int[m];
+            for (int i = fixed; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[j] += matrix[i][j];
+                }
+
+                ans += numSubarraySumTar(arr, target);
+            }
+        }
+
+        return ans;
+    }
+
+    private int numSubarraySumTar(int[] arr, int target) {
+        int n = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sof = 0;
+        int cnt = 0;
+
+        for (int ele : arr) {
+            sof += ele;
+            cnt += map.getOrDefault(sof - target, 0);
+            map.put(sof, map.getOrDefault(sof, 0) + 1);
+        }
+
+        return cnt;
+    }
 }
