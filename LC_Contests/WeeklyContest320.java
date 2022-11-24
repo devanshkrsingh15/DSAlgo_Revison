f7907d5ec9c2aa8e4fd4e58ec3f795b0fa817786
Devansh <kumardevansh22@gmail.com>
2022-11-24 10:19:38 +0530

    LC Contest

--- LC_Contests/WeeklyContest320.java
+++ LC_Contests/WeeklyContest320.java
@@ -18,8 +18,8 @@ public class WeeklyContest320 {
         return ans;
     }
 
-    //2476. Closest Nodes Queries in a Binary Search Tree
-     class TreeNode {
+    // 2476. Closest Nodes Queries in a Binary Search Tree
+    class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -39,13 +39,13 @@ public class WeeklyContest320 {
     }
 
     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
-        TreeSet<Integer>set = new TreeSet<>();
-        fill(set,root);
-        List<List<Integer>>ans=new ArrayList<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        fill(set, root);
+        List<List<Integer>> ans = new ArrayList<>();
         for (int tar : queries) {
-            List<Integer>tmp = new ArrayList<>();
-            int floor= set.floor(tar) == null ? -1 :set.floor(tar) ;
-            int ceil=set.ceiling(tar) == null ? -1 :set.ceiling(tar) ;
+            List<Integer> tmp = new ArrayList<>();
+            int floor = set.floor(tar) == null ? -1 : set.floor(tar);
+            int ceil = set.ceiling(tar) == null ? -1 : set.ceiling(tar);
             tmp.add(floor);
             tmp.add(ceil);
             ans.add(tmp);
@@ -55,12 +55,59 @@ public class WeeklyContest320 {
     }
 
     private void fill(TreeSet<Integer> set, TreeNode root) {
-        if(root==null) return ;
+        if (root == null)
+            return;
         set.add(root.val);
-        fill(set,root.left);
-        fill(set,root.right);
+        fill(set, root.left);
+        fill(set, root.right);
 
     }
 
-    
+    // 2477. Minimum Fuel Cost to Report to the Capital
+
+    long mincost = 0;
+
+    public long minimumFuelCost(int[][] roads, int seats) {
+        if (roads.length == 0)
+            return 0;
+        ArrayList<Integer> graph[] = createGraph(roads, seats);
+
+        boolean[] vis = new boolean[(int) 1e5 + 10];
+        _minimumFuelCost(graph, 0, seats, vis);
+
+        return mincost;
+    }
+
+    private long _minimumFuelCost(ArrayList<Integer>[] graph, int src, int seats, boolean[] vis) {
+        long size = 1l;
+        vis[src] = true;
+        for (int child : graph[src]) {
+
+            if (vis[child] == false) {
+                size += _minimumFuelCost(graph, child, seats, vis);
+            }
+        }
+
+        if (src != 0)
+            mincost += size / seats + (size % seats == 0 ? 0 : 1);//
+        return size;
+    }
+
+    private ArrayList<Integer>[] createGraph(int[][] roads, int seats) {
+        int n = roads.length;
+        ArrayList<Integer> graph[] = new ArrayList[(int) 1e5 + 10];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] ed : roads) {
+            int u = ed[0];
+            int v = ed[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        return graph;
+    }
+
 }
