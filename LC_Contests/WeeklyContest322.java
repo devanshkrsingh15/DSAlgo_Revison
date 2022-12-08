510e2959cb93642a1323420b7e85957f6d948d2c
Devansh <kumardevansh22@gmail.com>
2022-12-08 11:17:29 +0530

    LC Contest

--- LC_Contests/WeeklyContest322.java
+++ LC_Contests/WeeklyContest322.java
@@ -54,4 +54,70 @@ public class WeeklyContest322 {
         return ans;
 
     }
+
+    // 2492. Minimum Score of a Path Between Two Cities
+    public int minScore(int n, int[][] roads) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Edge>[] graph = buildGraph(n, roads, map);
+        boolean[] vis = new boolean[n + 1];
+        ArrayList<Integer> list = new ArrayList<>();
+
+        dfs(graph, 1, vis, list);
+        if (n == 1)
+            return 0;
+
+        int min = (int) 1e9;
+
+        for (int ele : list)
+            min = Math.min(min, map.get(ele));
+
+        return min;
+    }
+
+    private void dfs(ArrayList<Edge>[] graph, int src, boolean[] vis, ArrayList<Integer> list) {
+        vis[src] = true;
+        list.add(src);
+        for (Edge e : graph[src]) {
+            if (!vis[e.nbr]) {
+
+                dfs(graph, e.nbr, vis, list);
+            }
+        }
+    }
+
+    class Edge {
+        int nbr;
+        int cost;
+
+        Edge(int nbr, int cost) {
+            this.nbr = nbr;
+            this.cost = cost;
+        }
+
+    }
+
+    public ArrayList<Edge>[] buildGraph(int n, int[][] roads, HashMap<Integer, Integer> map) {
+        ArrayList<Edge>[] graph = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++)
+            graph[i] = new ArrayList<>();
+
+        for (int[] r : roads) {
+            int u = r[0];
+            int v = r[1];
+            int w = r[2];
+
+            map.putIfAbsent(u, w);
+            map.putIfAbsent(v, w);
+
+            if (w < map.get(u))
+                map.put(u, w);
+            if (w < map.get(v))
+                map.put(v, w);
+
+            graph[u].add(new Edge(v, w));
+            graph[v].add(new Edge(u, w));
+        }
+
+        return graph;
+    }
 }
