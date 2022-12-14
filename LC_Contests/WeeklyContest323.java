c26991df81cb12c637b99610f5b6a8d61280c9ab
Devansh <kumardevansh22@gmail.com>
2022-12-14 09:25:56 +0530

    LC Contest

--- LC_Contests/WeeklyContest323.java
+++ LC_Contests/WeeklyContest323.java
@@ -55,3 +55,75 @@ public class WeeklyContest323 {
     }
 
 }
+
+//2502. Design Memory Allocator
+class Allocator {
+
+    HashMap<Integer,ArrayList<Integer>>map;
+    boolean[]arr;
+    TreeSet<Integer>set;
+
+    public Allocator(int n) {
+        arr = new boolean[n];
+        map = new HashMap<>();
+        set= new TreeSet<>();
+        for(int i = 0;i<n;i++) set.add(i);
+    }
+    
+    public int allocate(int size, int mID) {
+          if (size > arr.length) {
+            return -1;
+        }
+        int findSt = getValidStartingPos(size);
+        if(findSt==-1) return -1;
+
+        map.putIfAbsent(mID,new ArrayList<>());
+        int pos =findSt;
+        while(size-->0){
+            arr[pos]=true;
+            map.get(mID).add(pos);
+            set.remove(pos);
+            pos++;
+        }
+
+        return findSt;
+    }
+
+    public int getValidStartingPos(int s){
+        boolean flag = false;
+        int pos = 0;
+        while(!flag){
+            int nextPos = -1;
+            int t = s;
+            int f = 0;
+            int cd = pos;
+            while(t-->0){
+                if(pos==arr.length || arr[pos]){
+                    f = 1;
+                    nextPos = pos;
+                    break;
+                }
+                pos++;
+            }
+            if(f==0) return cd;
+            pos = set.ceiling(nextPos+1)==null ? -1 :set.ceiling(nextPos+1);
+            if(pos==-1) return -1;
+        }
+
+       
+        return -1;
+    }
+    
+    public int free(int mID) {
+        if(!map.containsKey(mID)) return 0;
+        ArrayList<Integer>list = map.get(mID);
+        for(int ele:list){
+            arr[ele] = false;
+            set.add(ele);
+        }
+        
+        map.remove(mID);
+        return list.size();
+        
+    }
+}
