15275e41b798745f7252d4e2f678bab036c01626
Devansh <kumardevansh22@gmail.com>
2022-12-16 17:16:52 +0530

    LC Contest

--- LC_Contests/WeeklyContest323.java
+++ LC_Contests/WeeklyContest323.java
@@ -127,3 +127,102 @@ class Allocator {
         
     }
 }
+
+
+//2503. Maximum Number of Points From Grid Queries
+class Solution {
+    public int[] maxPoints(int[][] grid, int[] queries) {
+        int k = queries.length;
+        ArrayList<int[]>qs = new ArrayList<>();
+        for(int i = 0 ;i<k;i++){
+            qs.add(new int[]{i,queries[i]});
+        }
+
+        Collections.sort(qs,(a,b)->{
+            return a[1] - b[1];
+        });
+
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        HashSet<Integer>nextStPos =new HashSet<>();
+        HashMap<Integer,Integer>map = new HashMap<>();
+        nextStPos.add(0);
+
+        int[]ans = new int[k];
+        boolean vis[][] = new boolean[n][m];
+        int ansTillNow = 0;
+        for(int i =  0 ;i<k;i++ ){
+            
+            HashSet<Integer>n_nextStPos = new HashSet<>();
+            int[]a = qs.get(i);
+            int ele = a[1];
+            int idx = a[0];
+
+            if(map.containsKey(ele)){
+                ans[idx] = map.get(ele);
+                continue;                
+            }
+
+            if(grid[0][0] >= ele) continue;
+            
+            int tmp = 0;
+            for(int st :nextStPos ){
+                tmp += bfs(st,vis,grid,ele,n_nextStPos);
+            }
+            ans[idx] = tmp + ansTillNow;
+            map.put(ele,ans[idx]);
+            ansTillNow+=tmp;
+            nextStPos = n_nextStPos;
+            }
+
+        return  ans;
+    }
+
+
+    public int bfs(int idx,boolean vis[][],int[][] grid,int ele, HashSet<Integer>list ){
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int r= idx/m;
+        int c = idx%m;
+        ArrayDeque<Integer>q = new ArrayDeque<>();
+        if(grid[r][c]>=ele){
+            list.add(idx);
+            return 0;
+        }
+
+        q.add(idx);
+        int[][]direcs ={{-1,0},{0,-1},{1,0},{0,1}};
+        int ans =0;
+
+        while(q.size()!=0){
+            int s = q.size();
+
+            while(s-->0){
+                int ridx = q.remove();
+                r= ridx/m;
+                c = ridx%m;
+                if(grid[r][c]>=ele){ list.add(ridx); continue;}
+ 
+                ans++;
+                vis[r][c] = true;
+
+                for(int k = 0;k<direcs.length;k++){
+                    int x = direcs[k][0] + r;
+                    int y = direcs[k][1] + c;
+                    int nidx= x*m+y;
+                    if(x>=0 && y>=0 && x<n && y<m &&!vis[x][y]){
+                        vis[x][y]=true;
+                        if(ele>grid[x][y])q.add(nidx);
+                        else if(ele<=grid[x][y])list.add(nidx);
+                    }
+                }
+
+            }
+        }
+
+        return ans;
+    }
+}
