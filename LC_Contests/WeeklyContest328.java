caf918e3686162fff4dc33f1574860731082b907
Devansh <kumardevansh22@gmail.com>
2023-01-23 09:37:09 +0530

    LC Contest

---
+++ LC_Contests/WeeklyContest328.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class WeeklyContest328 {
+
+    // 2535. Difference Between Element Sum and Digit Sum of an Array
+    public int differenceOfSum(int[] nums) {
+        int sof = 0;
+        int dig = 0;
+
+        for (int ele : nums) {
+            sof += ele;
+            dig += getSum(ele);
+        }
+
+        return Math.abs(sof - dig);
+
+    }
+
+    private int getSum(int ele) {
+        int sof = 0;
+        while (ele != 0) {
+            sof += ele % 10;
+            ele /= 10;
+
+        }
+
+        return sof;
+    }
+
+    // 2536. Increment Submatrices by One
+
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int[][] ans = new int[n][n];
+
+        for (int[] q : queries) {
+            int sr = q[0];
+            int sc = q[1];
+            int er = q[2];
+            int ec = q[3];
+
+            for (int r = sr; r <= er; r++) {
+                ans[r][sc]++;
+                if (ec + 1 < n)
+                    ans[r][ec + 1]--;
+            }
+        }
+
+        for(int i  = 0 ; i<n;i++){
+            int sof  = 0;
+            for(int j = 0; j<n;j++){
+                sof+=ans[i][j];
+                ans[i][j]=sof;
+            }
+        }
+
+        return ans;
+    }
+}
--- LC_Daily.java
+++ LC_Daily.java
@@ -56,4 +56,26 @@ public class LC_Daily {
             return ans;
         }
     }
+
+
+    class LC997 {
+    public int findJudge(int n, int[][] trust) {
+        int[]indegree = new int[n+1];
+        int[]outdegree = new int[n+1];
+
+        for(int[]t :trust ){
+            int u = t[0];
+            int v = t[1];
+
+            indegree[v]++;
+            outdegree[u]++;
+        }
+
+        for(int i =1;i<=n;i++){
+            if(outdegree[i]==0 &&indegree[i]==n-1) return i;
+        }
+
+        return -1;
+    }
+}
 }
