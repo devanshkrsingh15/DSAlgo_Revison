559c1f9ef3e9d55509e10c0484ebe3ab11b9752c
Devansh <kumardevansh22@gmail.com>
2023-03-20 09:18:50 +0530

    LC Qs

--- LC_Daily.java
+++ LC_Daily.java
@@ -1900,3 +1900,18 @@ class WordDictionary {
  * obj.addWord(word);
  * boolean param_2 = obj.search(word);
  */
+
+ class LC605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0;i<flowerbed.length;i++){
+           if(flowerbed[i]==0){
+               if((i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                   flowerbed[i] = 1;
+                   n--;
+               }
+           }
+        }
+
+        return n<=0;
+    }
+}
