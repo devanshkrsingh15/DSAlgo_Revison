c3a4117e917ce6537721b302583122f5940d218e
Devansh <kumardevansh22@gmail.com>
2023-07-25 15:26:10 +0530

    LC Qs

--- LC_Daily.java
+++ LC_Daily.java
@@ -4,6 +4,25 @@ import java.util.*;
 
 public class LC_Daily {
 
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int lo = 0;
+        int hi = n - 1;
+        int ans = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid + 1] > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                ans = mid;
+                hi = mid - 1;
+            }
+        }
+
+        return ans;
+
+    }
+
     class LC131 {
         List<List<String>> ans = new ArrayList<>();
         boolean[][] isPal;
