4f94341e74f8c064a60c4c08570804d82f7afe54
Devansh <kumardevansh22@gmail.com>
2023-09-21 19:24:07 +0530

    LC Qs

--- LC_Contests/WeeklyContest362.java
+++ LC_Contests/WeeklyContest362.java
@@ -96,4 +96,98 @@ public class WeeklyContest362 {
 
         return ans == -1 ? (int) 1e9 : ans + 1;
     }
+
+    // LC 2850 Minimum Moves to Spread Stones Over Grid
+    public int minimumMoves(int[][] grid) {
+        int empty = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j] == 0)
+                    empty++;
+            }
+        }
+        if (empty == 0)
+            return 0;
+
+        int min = (int) 1e9;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j] == 0) {
+                    for (int ni = 0; ni < 3; ni++) {
+                        for (int nj = 0; nj < 3; nj++) {
+                            if (grid[ni][nj] > 1) {
+                                int moves = Math.abs(ni - i) + Math.abs(nj - j);
+
+                                grid[ni][nj]--;
+                                grid[i][j]++;
+
+                                min = Math.min(min, minimumMoves(grid) + moves);
+
+                                grid[ni][nj]++;
+                                grid[i][j]--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return min;
+    }
+
+    // 4. Median of Two Sorted Arrays
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // binary search on smaller array
+        // basically how many to choose from that, remaining from larger one
+
+        int n = nums1.length;
+        int m = nums2.length;
+        if (n > m)
+            return findMedianSortedArrays(nums2, nums1);
+        // considering nums1 to be smaller than nums2
+
+        int lo = 0; // no elements from nums1
+        int hi = n; // all elements from nums1
+
+        int total = (n + m + 1) / 2; // adding one so that odd can be handled, left part should have more elements
+
+        while (lo <= hi) {
+
+            int mid = lo + (hi - lo) / 2; // number of elements from array1
+            int mid2 = total - mid; // number of elements from array2
+
+            int l1 = -(int) 1e9;
+            int r1 = (int) 1e9;
+            int l2 = -(int) 1e9;
+            int r2 = (int) 1e9;
+
+            if (mid - 1 >= 0)
+                l1 = nums1[mid - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+
+            if (mid < n)
+                r1 = nums1[mid];
+            if (mid2 < m)
+                r2 = nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0)
+                    return (double) ((double) Math.max(l1, l2) + (double) Math.min(r1, r2)) / 2.0;
+                else
+                    return (double) ((double) Math.max(l1, l2));
+            } else if (l1 > r2) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+
+        }
+
+        return 0.0;
+
+    }
+
 }
