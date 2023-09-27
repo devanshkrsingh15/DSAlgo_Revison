0cd1ab4a3955ca76f4c1181b1915aa8d24c6d07d
Devansh <kumardevansh22@gmail.com>
2023-09-27 16:41:06 +0530

    LC Qs

--- LC_Contests/BiweeklyContest111.java
+++ LC_Contests/BiweeklyContest111.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.*;
 
 public class BiweeklyContest111 {
@@ -88,5 +89,81 @@ public class BiweeklyContest111 {
     }
 
     // 2827. Number of Beautiful Integers in the Range
+
+    // 880. Decoded String at Index
+    public String decodeAtIndex(String s, int K) {
+        long finalLen = 0l;
+        for (int i = 0; i < s.length(); i++) {
+            char chi = s.charAt(i);
+            finalLen = chi >= '2' && chi <= '9' ? finalLen * (long) (chi - '0') : finalLen + 1l;
+        }
+
+        long k = (long) K;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char chi = s.charAt(i);
+
+            if (chi >= '2' && chi <= '9') {
+                finalLen /= (int) (chi - '0');
+                k = k % finalLen;
+            } else {
+
+                if (k == 0 || (long) k == finalLen)
+                    return "" + s.charAt(i);
+                finalLen--;
+            }
+        }
+
+        return "";
+
+    }
+
+    // 2719. Count of Integers
+    public int count(String num1, String num2, int min_sum, int max_sum) {
+        String l = num2;
+        String r = getLeft(num1);
+        dp = new Long[50][3][500];
+
+        long a = count_(l, 0, true, 0, min_sum, max_sum);
+        dp = new Long[50][3][500];
+        long b = count_(r, 0, true, 0, min_sum, max_sum);
+
+        return (int) ((a % mod - b % mod + mod) % mod);
+
+    }
+
+    Long[][][] dp;
+
+    long mod = (long) 1e9 + 7;
+
+    public long count_(String s, int pos, boolean bound, int sum, int min_sum, int max_sum) {
+        if (pos == s.length()) {
+            return min_sum <= sum && sum <= max_sum ? 1l : 0l;
+        }
+
+        if (dp[pos][bound ? 1 : 0][sum] != null)
+            return dp[pos][bound ? 1 : 0][sum];
+
+        long ans = 0l;
+
+        int max = 9;
+        if (bound) {
+            max = (int) (s.charAt(pos) - '0');
+        }
+
+        for (int i = 0; i <= max; i++) {
+            int nsum = sum + i;
+            ans = (ans % mod + count_(s, pos + 1, bound && (i == max), nsum, min_sum, max_sum) % mod) % mod;
+
+        }
+
+        return dp[pos][bound ? 1 : 0][sum] = ans % mod;
+    }
+
+    public String getLeft(String s) {
+        BigInteger l = new BigInteger(s);
+        BigInteger sl = l.subtract(new BigInteger("1"));
+        return sl.toString();
+    }
 
 }
